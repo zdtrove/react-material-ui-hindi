d0496d59bd955e55c183dc2827d669a053e3f9ae
Hoang An <antnh@icd-vn.com>
2021-06-29 16:54:05 +0700

    cm

---
+++ src/components/MuiBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '@material-ui/core'
+
+const MuiBox = () => {
+    return (
+        <Box width={500} boxShadow={2} p={3} component="section">
+            <h1>Get started</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quia iure sint facilis optio vero voluptatem porro, quidem labore, illo quae ipsam. Vero doloremque praesentium nemo est a nesciunt vel.</p>
+        </Box>
+    )
+}
+
+export default MuiBox
